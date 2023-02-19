a4183dca8242b8562729d8720ef330654889b91a
Burak Taha Cevheroğlu <lunaslol18@gmail.com>
2023-02-19 03:09:16 +0300

    some code improvement

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -10,19 +10,23 @@ class MainPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('${ref.watch(statusProvider)}'),
+        title: Text(ref.read(statusProvider).toString()),
       ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 10),
-          width: double.infinity,
-          height: 300,
-          color: Colors.grey,
-          child: Stack(
-            clipBehavior: Clip.none,
-            children: charStatus(ref.watch(statusProvider))
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            width: double.infinity,
+            height: 300,
+            color: Colors.grey,
+            child: Stack(
+                children: charStatus(ref.watch(statusProvider))),
           ),
-        ),
+          const SizedBox(
+            height: 100,
+          ),
+          const BottomButtons(),
+        ],
       ),
     );
   }
@@ -30,12 +34,13 @@ class MainPage extends ConsumerWidget {
 
 class CharItemWidget extends StatelessWidget {
   const CharItemWidget({
-    super.key, required this.placement, required this.char,
+    super.key,
+    required this.placement,
+    required this.char,
   });
-  
+
   final Placement placement;
   final Characters char;
-  
 
   @override
   Widget build(BuildContext context) {
@@ -48,20 +53,20 @@ class CharItemWidget extends StatelessWidget {
         height: charSize(placement),
         decoration: BoxDecoration(
             image: DecorationImage(
-                colorFilter: charEffect(placement),
+                colorFilter: charEffect(placement), 
                 image: character(char))),
       ),
     );
   }
 }
 
-enum Placement { first, second, third, fourth}
-enum Characters { char1, char2, char3, char4}
-enum Status { first, second, third, fourth}
+enum Placement { first, second, third, fourth }
+enum Characters { char1, char2, char3, char4 }
+enum Status { first, second, third, fourth }
 const _duration = Duration(milliseconds: 300);
 
-AssetImage character(Characters char){
-  switch(char){
+AssetImage character(Characters char) {
+  switch (char) {
     case Characters.char1:
       return const AssetImage("assets/char1.png");
     case Characters.char2:
@@ -72,32 +77,32 @@ AssetImage character(Characters char){
       return const AssetImage("assets/char4.png");
   }
 }
-AlignmentGeometry charPlacement(Placement placement){
-  switch(placement) {
+AlignmentGeometry charPlacement(Placement placement) {
+  switch (placement) {
     case Placement.first:
       return Alignment.bottomCenter;
     case Placement.second:
       return Alignment.centerRight;
     case Placement.third:
-      return Alignment.centerLeft;
-    case Placement.fourth:
       return Alignment.topCenter;
+    case Placement.fourth:
+      return Alignment.centerLeft;
   }
 }
-double charSize(Placement placement){
-  switch(placement) {
+double charSize(Placement placement) {
+  switch (placement) {
     case Placement.first:
       return 200;
     case Placement.second:
       return 130;
     case Placement.third:
-      return 130;
-    case Placement.fourth:
       return 110;
+    case Placement.fourth:
+      return 130;
   }
 }
-ColorFilter? charEffect(Placement placement){
-  switch(placement) {
+ColorFilter? charEffect(Placement placement) {
+  switch (placement) {
     case Placement.first:
       return null;
     case Placement.second:
@@ -108,8 +113,8 @@ ColorFilter? charEffect(Placement placement){
       return ColorFilter.mode(Colors.white.withAlpha(150), BlendMode.modulate);
   }
 }
-List<Widget> charStatus(Status status){
-  switch(status){
+List<Widget> charStatus(Status status) {
+  switch (status) {
     case Status.first:
       return CharacterLines().first;
     case Status.second:
@@ -121,50 +126,56 @@ List<Widget> charStatus(Status status){
   }
 }
 
-
-
-
 class BottomButtons extends ConsumerWidget {
-  const BottomButtons({
-    super.key, required this.bottom
-  });
-
-  final double bottom;
+  const BottomButtons({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Positioned(
-        bottom: bottom,
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(onPressed: (){
-                ref.read(statusProvider.notifier).update((state) => Status.first);
-              }, child: const Text('1')),
-              ElevatedButton(onPressed: (){
-                ref.read(statusProvider.notifier).update((state) => Status.second);
-              }, child: const Text('2')),
-              ElevatedButton(onPressed: (){
-                ref.read(statusProvider.notifier).update((state) => Status.third);
-              }, child: const Text('3')),
-              ElevatedButton(onPressed: (){
-                ref.read(statusProvider.notifier).update((state) => Status.fourth);
-              }, child: const Text('4'))
-      ],
-    ),
-        ));
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                ref
+                    .read(statusProvider.notifier)
+                    .update((state) => Status.first);
+              },
+              child: const Text('1')),
+          ElevatedButton(
+              onPressed: () {
+                ref
+                    .read(statusProvider.notifier)
+                    .update((state) => Status.second);
+              },
+              child: const Text('2')),
+          ElevatedButton(
+              onPressed: () {
+                ref
+                    .read(statusProvider.notifier)
+                    .update((state) => Status.third);
+              },
+              child: const Text('3')),
+          ElevatedButton(
+              onPressed: () {
+                ref
+                    .read(statusProvider.notifier)
+                    .update((state) => Status.fourth);
+              },
+              child: const Text('4'))
+        ],
+      ),
+    );
   }
 }
 
-class CharacterLines{
+class CharacterLines {
   List<Widget> first = const [
     CharItemWidget(placement: Placement.fourth, char: Characters.char4),
     CharItemWidget(placement: Placement.third, char: Characters.char3),
     CharItemWidget(placement: Placement.second, char: Characters.char2),
     CharItemWidget(placement: Placement.first, char: Characters.char1),
-    BottomButtons(bottom: -100,)
   ];
 
   List<Widget> second = const [
@@ -172,7 +183,6 @@ class CharacterLines{
     CharItemWidget(placement: Placement.third, char: Characters.char2),
     CharItemWidget(placement: Placement.second, char: Characters.char1),
     CharItemWidget(placement: Placement.first, char: Characters.char4),
-    BottomButtons(bottom: -100,)
   ];
 
   List<Widget> third = const [
@@ -180,7 +190,6 @@ class CharacterLines{
     CharItemWidget(placement: Placement.third, char: Characters.char1),
     CharItemWidget(placement: Placement.second, char: Characters.char4),
     CharItemWidget(placement: Placement.first, char: Characters.char3),
-    BottomButtons(bottom: -100,)
   ];
 
   List<Widget> fourth = const [
@@ -188,7 +197,5 @@ class CharacterLines{
     CharItemWidget(placement: Placement.third, char: Characters.char4),
     CharItemWidget(placement: Placement.second, char: Characters.char3),
     CharItemWidget(placement: Placement.first, char: Characters.char2),
-    BottomButtons(bottom: -100,)
   ];
-
 }
